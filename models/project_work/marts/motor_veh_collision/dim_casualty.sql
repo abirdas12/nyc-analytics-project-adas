507e39ae7ff171c9593ad1d37c1ab1f5aaf6d1f7
Abir Das <abirdasusa12@gmail.com>
2026-04-22 00:51:35 +0000

    Adjusted the field names to match the model.

--- models/project_work/marts/motor_veh_collision/dim_casualty.sql
+++ models/project_work/marts/motor_veh_collision/dim_casualty.sql
@@ -1,8 +1,8 @@
 
 WITH casualty AS (
     SELECT DISTINCT 
-         CAST(number_of_persons_injured AS INT) AS number_of_persons_injured 
-        ,CAST(number_of_persons_killed AS INT)  AS number_of_persons_killed
+         CAST(number_of_persons_injured AS INT) AS persons_injured 
+        ,CAST(number_of_persons_killed AS INT)  AS persons_killed
     FROM {{ref('staging_tbl_veh_collision')}}
     WHERE number_of_persons_killed IS NOT NULL 
         OR number_of_persons_injured  IS NOT NULL
@@ -11,9 +11,9 @@ WITH casualty AS (
 
 ,dim_casualty AS (
     SELECT 
-        {{ dbt_utils.generate_surrogate_key(['number_of_persons_injured', 'number_of_persons_killed']) }} AS casualty_key
-        ,number_of_persons_injured 
-        ,number_of_persons_killed
+        {{ dbt_utils.generate_surrogate_key(['persons_injured', 'persons_killed']) }} AS casualty_key
+        ,persons_injured 
+        ,persons_killed
     FROM casualty
 )
 
