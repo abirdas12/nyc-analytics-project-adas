2214b2ecd500d151e6546cc94e69741bf6ff2d7e
Abir Das <abirdasusa12@gmail.com>
2026-04-22 01:19:25 +0000

    Had to format the crash_time in HH:MM:SS in the dim_time SQL code.

--- models/project_work/marts/motor_veh_collision/dim_time.sql
+++ models/project_work/marts/motor_veh_collision/dim_time.sql
@@ -1,6 +1,6 @@
 WITH time AS (
     SELECT DISTINCT 
-        CAST(crash_time AS TIME) AS crash_time
+        CAST(PARSE_TIME('%H:%M', crash_time) AS TIME) AS crash_time
     FROM {{ref('staging_tbl_veh_collision')}}
     WHERE crash_time IS NOT NULL 
 
