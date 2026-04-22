2eadde29816443f26cde0c432692b10e968a57f2
Abir Das <abirdasusa12@gmail.com>
2026-04-22 00:23:34 +0000

    Created the SQL code for dim_casualty

---
+++ models/project_work/marts/motor_veh_collision/dim_casualty.sql
@@ -0,0 +1,21 @@
+
+WITH casualty AS (
+    SELECT DISTINCT 
+         CAST(number_of_persons_injured AS INT) AS number_of_persons_injured 
+        ,CAST(number_of_persons_killed AS INT)  AS number_of_persons_killed
+    FROM {{ref("staging_tbl_veh_collision")}}
+    WHERE number_of_persons_killed IS NOT NULL 
+        OR number_of_persons_injured  IS NOT NULL
+
+)
+
+,dim_casualty AS (
+    SELECT 
+        {{ dbt_utils.generate_surrogate_key(['number_of_persons_injured', 'number_of_persons_killed']) }} AS casualty_key
+        ,number_of_persons_injured 
+        ,number_of_persons_killed
+    FROM dim_casualty
+)
+
+SELECT *
+FROM dim_casualty
