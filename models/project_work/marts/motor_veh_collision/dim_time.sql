2a021d2d113eddc17be59290e392711ff916ca4d
Abir Das <abirdasusa12@gmail.com>
2026-04-25 15:05:44 +0000

    Wrote some comments for the dim_time SQL code.

--- models/project_work/marts/motor_veh_collision/dim_time.sql
+++ models/project_work/marts/motor_veh_collision/dim_time.sql
@@ -1,6 +1,6 @@
 WITH time AS (
     SELECT DISTINCT 
-        CAST(PARSE_TIME('%H:%M', crash_time) AS TIME) AS crash_time
+        CAST(PARSE_TIME('%H:%M', crash_time) AS TIME) AS crash_time  -- converting crash time to HH:MM:SS
     FROM {{ref('staging_tbl_veh_collision')}}
     WHERE crash_time IS NOT NULL 
 
@@ -12,8 +12,8 @@ WITH time AS (
             'EXTRACT(HOUR FROM crash_time)'
             ,'EXTRACT(MINUTE FROM crash_time)'
         ]) }} AS time_key
-        ,CAST(COALESCE(EXTRACT(HOUR FROM crash_time),0) AS INT)   AS hour
-        ,CAST(COALESCE(EXTRACT(MINUTE FROM crash_time),0) AS INT) AS minute
+        ,CAST(COALESCE(EXTRACT(HOUR FROM crash_time),0) AS INT)   AS hour -- extracting the hour from the time and replacing the NULLs with 0
+        ,CAST(COALESCE(EXTRACT(MINUTE FROM crash_time),0) AS INT) AS minute -- extracting the minute from the time and replacing the NULLs with 0
     FROM time
 )
 
