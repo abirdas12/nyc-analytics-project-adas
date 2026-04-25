8bf698c9e090757ad44fb759a98e5ca07ca3c1a6
Abir Das <abirdasusa12@gmail.com>
2026-04-25 13:59:41 +0000

    Added comment for dim_casualty.

--- models/project_work/marts/motor_veh_collision/dim_casualty.sql
+++ models/project_work/marts/motor_veh_collision/dim_casualty.sql
@@ -1,4 +1,6 @@
 
+-- Setting up the dimension table for dim_casualty with the below SQL code. 
+
 WITH casualty AS (
     SELECT DISTINCT 
          CAST(number_of_persons_injured AS INT) AS persons_injured 
